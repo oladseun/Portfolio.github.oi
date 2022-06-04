61e5ff8894a4ad06bb5e12662edd5f31bdeef20e
Oluwaseun Joseph Oladeji <96907647+oladseun@users.noreply.github.com>
2022-06-04 04:34:06 +0100

    Add files via upload

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://127.0.0.1/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://127.0.0.1/";
+		</script>
+
+		<p>You are being redirected to <a href="http://127.0.0.1/">http://127.0.0.1/</a></p>
+	</body>
+</html>
